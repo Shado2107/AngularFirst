acd4b94507a47a1e07e990e0da7ccf83eb944235
Shado2107 <pd39almeida@outlook.com>
2022-10-23 16:48:53 +0000

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [
-  {path: '/home', component: HomeComponent},
+const BlogRoutesList: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  }
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(BlogRoutesList)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
